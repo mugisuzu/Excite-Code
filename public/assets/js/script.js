0b4fc8cdb2f9a6696ea574fc01bc39c829f2b24d
mugi <mugisuzu222@gmail.com>
2024-01-07 14:01:32 +0900

    　first commit

---
+++ public/assets/js/script.js
@@ -0,0 +1,42 @@
+// ハンバーガーメニュー
+const burger = document.querySelector('#js-hamburger');
+const spMenu = document.querySelector('#js-spMenu');
+const body = document.querySelector('body');
+
+if (burger) {
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('is-active');
+    spMenu.classList.toggle('show');
+    if (burger.classList.contains('is-active')) {
+      body.style.overflow = 'hidden';
+    } else {
+      body.style.overflow = 'visible';
+    }
+  });
+}
+
+
+// fadeAnimation
+function fadeAnimation(el,cl) {
+  $(el).each(function () {
+    let elemPos = $(this).offset().top - 50;
+    let scroll = $(window).scrollTop();
+    let windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass(cl);
+    }
+  });
+}
+
+// 画面をスクロールをしたら発火
+$(window).scroll(function () {
+  fadeAnimation('.fadeUpTrigger','fadeUp'); /* アニメーション用の関数を呼ぶ*/
+  fadeAnimation('.fadeLeftTrigger','fadeLeft'); /* アニメーション用の関数を呼ぶ*/
+  fadeAnimation('.fadeRightTrigger','fadeRight'); /* アニメーション用の関数を呼ぶ*/
+  fadeAnimation('.fadeDownTrigger','fadeDown'); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面をロードしたら発火
+$(window).on('load', function () {
+  fadeAnimation('.logged-in', 'fadeIn'); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面をスクロールをしたら動かしたい場合の記述
